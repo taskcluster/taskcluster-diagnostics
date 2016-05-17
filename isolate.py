e1671ecb9edca77d792cb689ed3678ad089a6230
Chinmay Kousik <chinmaykousik1@gmail.com>
2016-05-18 01:40:39 +0530

    Easy tool to isolate test to run

---
+++ isolate.py
@@ -0,0 +1,36 @@
+import os
+import re
+import sys
+
+rootdir = 'src/diagnostics/'
+p = re.compile(r'(.+)_test.js$')
+q = re.compile(r'(.+)_iso.js$')
+
+def isolate_test(fname = None):
+    for path, s, files in os.walk(rootdir):
+        for file in files:
+            if file != fname:
+                if p.match(str(file)):
+                    new_name = p.match(str(file)).group(1) +'_iso.js'
+                    os.rename(os.path.join(path, file), os.path.join(path,new_name))
+
+def un_isolate_test(fname = None):
+    for path, s, files in os.walk(rootdir):
+        for file in files:
+            if q.match(str(file)):
+                new_name = q.match(str(file)).group(1) +'_test.js'
+                os.rename(os.path.join(path, file), os.path.join(path,new_name))
+
+
+def main():
+    f = isolate_test
+    if len(sys.argv) == 1:
+        sys.exit(0)
+    if sys.argv[1] == '1':
+        f = un_isolate_test
+    if len(sys.argv) == 3:
+        f(sys.argv[2])
+    else:
+        f(sys.argv[1])
+
+main()
--- src/diagnostics/worker_test.js
+++ src/diagnostics/worker_test.js
@@ -3,6 +3,7 @@ describe('Worker', function () {
   var taskcluster = require('taskcluster-client');
   var helper      = require('./helper');
   var slugid      = require('slugid');
+  var debug       = require('debug')('worker:test');
 
   it("should create and complete a task", function (done) {
     this.timeout(60*1000);
@@ -14,14 +15,15 @@ describe('Worker', function () {
     'taskRunning',
     'taskCompleted',
     'artifactCreated',
-    'taskCompleted'].map(exch => {
+    'taskCompleted'].forEach(exch => {
       let r = helper.queueEvents[exch]({ taskId });
       helper.listener.bind(r);
 
       promise[exch] = new Promise((resolve, reject) => {
         helper.listener.on('message', message => {
           if(message.exchange === helper.queueEvents[exch]().exchange){
-            debug(exch,': ',message.payload);
+            console.log(message.exchange);
+            console.log(exch,': ',message.payload);
             return resolve(message.payload);
           }
         }).on('error', () => {
