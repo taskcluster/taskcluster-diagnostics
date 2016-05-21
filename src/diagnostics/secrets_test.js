af81c742ff0d8ba55fbfc46d4fef1bfc57a3154d
Chinmay Kousik <chinmaykousik1@gmail.com>
2016-05-22 00:05:28 +0530

    Revert "Enough tests to know secrets isn't terribly broken"
    This reverts commit c9b9b545b4c673206290eec0cf8f90ba42478c79.
    
    	renamed:    src/diagnostics/auth_iso.js -> src/diagnostics/auth_test.js
    	renamed:    src/diagnostics/index_iso.js -> src/diagnostics/index_test.js
    	renamed:    src/diagnostics/queue_iso.js -> src/diagnostics/queue_test.js
    	modified:   src/diagnostics/secrets_test.js
    	renamed:    src/diagnostics/worker_iso.js -> src/diagnostics/worker_test.js

--- src/diagnostics/secrets_test.js
+++ src/diagnostics/secrets_test.js
@@ -23,7 +23,7 @@ describe("Secrets", function () {
     secret:   { data },
     expires:  taskcluster.fromNowJSON('- 2 hours')
   };
-  
+
   it('should write a secret (allowed) and retreive it', function (done) {
     this.timeout(30*1000);
     return helper.secrets.set("garbage/"+key1, payloadExpires)
@@ -80,35 +80,5 @@ describe("Secrets", function () {
       done();
     });
   });
-
-  it('should delete secret', function (done) {
-    this.timeout(60*1000);
-    return helper.secrets.set("garbage/"+key1, payloadExpires)
-    .then(() => {
-      return helper.secrets.remove("garbage/"+key1);
-    }).then(() => {
-      return helper.secrets.get("garbage/"+key1);
-    }).catch(err => {
-      debug("Status code: ",err.statusCode);
-      assert(err.statusCode === 404, "Wrong status code");
-      done();
-    }).then(() => {
-      throw Error("Secret should be deleted");
-    });
-  });
-
-  it('should fail when reading an expired secret', function (done) {
-    this.timeout(30*1000);
-    return helper.secrets.set("garbage/"+key2, payloadExpired)
-    .then(() => {
-      return helper.secrets.get("garbage/"+key2);
-    }).catch(err => {
-      debug("Error statusCode", err.statusCode);
-      assert(err.statusCode === 410);
-      done();
-    }).then(() => {
-      throw new Error("Secret should not be retreivable");
-    });
-  })
 
 });
