5c418919ecbbe8068b22507b10ed04a87ca23f57
Chinmay Kousik <chinmaykousik1@gmail.com>
2016-05-24 01:14:55 +0530

    Failing task test

--- src/diagnostics/worker_test.js
+++ src/diagnostics/worker_test.js
@@ -1,9 +1,11 @@
 'use strict';
 describe('Worker', function () {
-  var taskcluster = require('taskcluster-client');
+  var taskcluster = require('taskclusterclient');
   var helper      = require('./helper');
   var slugid      = require('slugid');
   var debug       = require('debug')('worker:test');
+  var assert      = require('assert');
+  var request     = require('superagent-promise')
 
   it("should create and complete a task", function (done) {
     this.timeout(120*1000);
@@ -20,9 +22,6 @@ describe('Worker', function () {
       promise[exch] = new Promise((resolve, reject) => {
         return helper.listener.on('message', message => {
           if(message.exchange === r.exchange){
-            if(exch === 'taskCompleted'){
-              done();
-            }
             debug(message.exchange);
             return resolve(message);
           }
@@ -38,17 +37,51 @@ describe('Worker', function () {
       return helper.queue.scheduleTask(taskId);
     }).then(() => {
       return promise.taskPending;
-    }).then(pendingResult => {
-      return helper.queue.claimTask(taskId, 0, {
-        workerGroup: 'dummy-test-workergroup',
-        workerId: 'dummy-test-worker-id'
-      });
     }).then(() => {
       return promise.taskRunning;
     }).then(() => {
       return promise.taskCompleted;
-    }).then(done);
+    }).then(() => {
+      done();
+    });
   });
+
+  it('should create and fail a task', function (done) {
+    this.timeout(120*1000);
+    let taskId = slugid.nice();
+    let promise = {};
+    debug("TaskId", taskId);
+
+    ['taskDefined',
+    'taskPending',
+    'taskFailed'].forEach(exch => {
+      let r = helper.queueEvents[exch]({ taskId });
+      helper.listener.bind(r);
+      promise[exch] = new Promise((resolve, reject) => {
+        return helper.listener.on('message', message => {
+          if(message.exchange === r.exchange){
+            debug(message.exchange);
+            return resolve(message);
+          }
+        });
+      });
+    });
+
+    return helper.listener.resume().then(() => {
+      return helper.queue.defineTask(taskId, helper.exitTaskDefEnvVars(taskId,1));
+    }).then(() => {
+      return promise.taskDefined;
+    }).then(() => {
+      return helper.queue.scheduleTask(taskId);
+    }).then(() => {
+      return promise.taskPending;
+    }).then(() => {
+      return promise.taskFailed;
+    }).then(() => {
+      done();
+    })
+  });
+
 
   after(function () {
     helper.listener.close();
