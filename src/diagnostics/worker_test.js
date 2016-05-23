2edfbf458956072fa0f462e9c4f8b751bd53f344
Chinmay Kousik <chinmaykousik1@gmail.com>
2016-05-24 00:54:33 +0530

    Revert "Added more worker tests"
    This reverts commit f52809939ad89d3c551f180f1be34f4094a7dccb.
    
    	modified:   package.json
    	renamed:    src/diagnostics/auth_iso.js -> src/diagnostics/auth_test.js
    	modified:   src/diagnostics/helper.js
    	modified:   src/diagnostics/index_test_unfinished.js
    	renamed:    src/diagnostics/queue_iso.js -> src/diagnostics/queue_test.js
    	renamed:    src/diagnostics/secrets_iso.js -> src/diagnostics/secrets_test.js
    	modified:   src/diagnostics/worker_test.js

--- src/diagnostics/worker_test.js
+++ src/diagnostics/worker_test.js
@@ -3,97 +3,25 @@ describe('Worker', function () {
   var taskcluster = require('taskcluster-client');
   var helper      = require('./helper');
   var slugid      = require('slugid');
-  var assert      = require('assert');
   var debug       = require('debug')('worker:test');
-  var request     = require('superagent-promise');
 
   it("should create and complete a task", function (done) {
     this.timeout(120*1000);
     let taskId = slugid.nice();
     let promise = {};
+    let runId, workerId, workerGroup;
     debug("TaskId", taskId);
     ['taskDefined',
     'taskPending',
+    'taskRunning',
     'taskCompleted'].forEach(exch => {
       let r = helper.queueEvents[exch]({ taskId });
       helper.listener.bind(r);
       promise[exch] = new Promise((resolve, reject) => {
         return helper.listener.on('message', message => {
           if(message.exchange === r.exchange){
-            debug(message.exchange);
-            return resolve(message);
-          }
-        });
-      });
-    });
-
-    return helper.listener.resume().then(() => {
-      return helper.queue.defineTask(taskId, helper.simpleTaskDef(taskId));
-    }).then(() => {
-      return promise.taskDefined;
-    }).then(() => {
-      return helper.queue.scheduleTask(taskId);
-    }).then(() => {
-      return promise.taskPending;
-    }).then(() => {
-      return promise.taskCompleted;
-    }).then(() => {
-      done();
-    });
-  });
-
-  it('should create and fail a task', function (done) {
-    this.timeout(120*1000);
-    let taskId = slugid.nice();
-    let promise = {};
-    debug("TaskId", taskId);
-
-    ['taskDefined',
-    'taskPending',
-    'taskFailed'].forEach(exch => {
-      let r = helper.queueEvents[exch]({ taskId });
-      helper.listener.bind(r);
-      promise[exch] = new Promise((resolve, reject) => {
-        return helper.listener.on('message', message => {
-          if(message.exchange === r.exchange){
-            debug(message.exchange);
-            return resolve(message);
-          }
-        });
-      });
-    });
-
-    return helper.listener.resume().then(() => {
-      return helper.queue.defineTask(taskId, helper.exitTaskDefEnvVars(taskId,1));
-    }).then(() => {
-      return promise.taskDefined;
-    }).then(() => {
-      return helper.queue.scheduleTask(taskId);
-    }).then(() => {
-      return promise.taskPending;
-    }).then(() => {
-      return promise.taskFailed;
-    }).then(() => {
-      done();
-    })
-  });
-
-  it('should retrieve created artifacts', function (done) {
-    this.timeout(120*1000);
-    let taskId = slugid.nice();
-    let promise = {};
-    debug("TaskId", taskId);
-    ['taskDefined',
-    'taskPending',
-    'artifactCreated',
-    'taskCompleted'].forEach(exch => {
-      let r = helper.queueEvents[exch]({ taskId });
-      helper.listener.bind(r);
-      promise[exch] = new Promise((resolve, reject) => {
-        return helper.listener.on('message', message => {
-          if(message.exchange === r.exchange){
-            if(exch === 'artifactCreated'){
-              debug(message);
+            if(exch === 'taskCompleted'){
+              done();
             }
             debug(message.exchange);
             return resolve(message);
@@ -108,23 +36,19 @@ describe('Worker', function () {
       return promise.taskDefined;
     }).then(() => {
       return helper.queue.scheduleTask(taskId);
-    }). then(() => {
-      return promise.taskPending;
-    })
-    .then(() => {
-      return promise.taskCompleted;
     }).then(() => {
-      let testArtifactName = 'public/logs/live.log'
-      let url = helper.queue.buildUrl(helper.queue.getArtifact, taskId, 0, testArtifactName);
-      debug(url);
-      return request.get(url).end().then(response => {
-        debug("Response status code:", response.statusCode);
-        assert(response.statusCode === 200, "Wrong status code");
-        debug(response.text);
-        done();
-      }).catch(done)
-    });
-  })
+      return promise.taskPending;
+    }).then(pendingResult => {
+      return helper.queue.claimTask(taskId, 0, {
+        workerGroup: 'dummy-test-workergroup',
+        workerId: 'dummy-test-worker-id'
+      });
+    }).then(() => {
+      return promise.taskRunning;
+    }).then(() => {
+      return promise.taskCompleted;
+    }).then(done);
+  });
 
   after(function () {
     helper.listener.close();
