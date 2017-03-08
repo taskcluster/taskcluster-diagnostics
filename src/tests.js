1f31604d0ab57cd6fa90cd8d28a317262f42239a
Brian Stack <bstack@mozilla.com>
2017-03-08 11:14:56 -0800

    Move to yarn and fix lint

--- src/tests.js
+++ src/tests.js
@@ -1,18 +1,18 @@
-'use strict';
+
 var TestRunner    = require('./runner/TestRunner');
 var debug         = require('debug')('diagnostics:test-server'); 
 
-process.send = process.send || function (args) {};
+process.send = process.send || function(args) {};
 
 var run = async () => {
   let tr = TestRunner.createTestRunner();
-  try{
+  try {
     let result = await tr.runTests();
     process.send(result);
   } catch (err) {
     console.error(err);
     process.send(null);
   }
-}
+};
 
 run();
